ec9f8502ce3f5aee5ff65a05889795d361a1db11
Akhil Antony <Akhil Antony@DESKTOP-1CDDNFL>
2021-11-17 02:19:09 +0530

    Add project files.

---
+++ type/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace type
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Type a = typeof(string);
+            Type b = typeof(int);
+            Type c = typeof(float);
+            System.Diagnostics.Debug.WriteLine(a.FullName);
+            System.Diagnostics.Debug.WriteLine(b.FullName);
+            System.Diagnostics.Debug.WriteLine(c.FullName);
+        }
+    }
+}
